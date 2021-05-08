406212bde41f381bc07dcebea0b2708d498d89c2
sena-anny <cozykoji18@gmail.com>
2021-05-08 20:46:02 +0900

    Meta/Footerコンポーネント作成

---
+++ components/Meta.tsx
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+
+export const Meta = (): JSX.Element => (
+  <Head>
+    <title>Joyful 支援日誌管理システム</title>
+    <link rel="icon" href="/joyful-logo.png" />
+  </Head>
+)
