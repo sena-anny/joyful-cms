468265eabc0caabbdd946678a2f4d16b8c3555cd
sena-anny <cozykoji18@gmail.com>
2021-05-30 05:06:06 +0900

    Firestoreへの登録部分実装

--- components/Form.tsx
+++ components/Form.tsx
@@ -9,6 +9,9 @@ import {
   getRegister,
   getTarget,
 } from '@utils/repositories/fetchDataFromClientSide'
+import { DialogWrapper } from '@components/DialogWrapper'
+import { PostModel } from '@utils/entities/PostModel'
+import { registerPost } from '@utils/repositories/postData'
 
 export const Form = ({
   title,
@@ -33,6 +36,8 @@ export const Form = ({
   } = useForm<IPostInputs>()
   const [targetName, setTargetName] = useState<string>('')
   const [registerName, setRegisterName] = useState<string>('')
+  const [openDialog, setDialog] = useState<boolean>(false)
+  const [alert, setAlert] = useState<string>('')
 
   useEffect(() => {
     const fetchUserName = async () => {
@@ -49,8 +54,21 @@ export const Form = ({
   }, [title, content])
 
   const registerFirebase = async (data: IPostInputs) => {
-    console.log(data)
-
+    const post: PostModel = {
+      id: `${targetDate}-${targetId}-${registerId}`,
+      title: data.title,
+      date: targetDate,
+      content: data.content,
+      targetId: targetId,
+      registerId: registerId,
+    }
+    const result = await registerPost(post)
+    setDialog(true)
+    if (result === 'success') {
+      setAlert(`登録完了しました`)
+    } else {
+      setAlert(`登録失敗しました。`)
+    }
     return
   }
 
@@ -59,80 +77,88 @@ export const Form = ({
   }
 
   return (
-    <form onSubmit={handleSubmit(registerFirebase)} className={styles.search}>
-      <div className={styles.form}>
-        <p>支援実施日</p>
-        {formatDate(targetDate)}
-      </div>
-      <div className={styles.form}>
-        <InputLabel shrink>支援対象者</InputLabel>
-        <p>{targetName}</p>
-      </div>
-      <div className={styles.form}>
-        <InputLabel shrink>入力ユーザー</InputLabel>
-        <p>{registerName}</p>
-      </div>
-      {!isLoading && (
-        <Controller
-          name="title"
-          control={control}
-          rules={{ required: true }}
-          defaultValue={title}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="支援場面"
-              variant="outlined"
-              placeholder="*必須"
-              margin="normal"
-              helperText="（例）母の日カード作り"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          )}
-        />
-      )}
-      {errors.title && <p className={styles.warn}>入力してください</p>}
-      {!isLoading && (
-        <Controller
-          name="content"
-          control={control}
-          rules={{ required: true }}
-          defaultValue={content}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              label="内容"
-              variant="outlined"
-              placeholder="*必須"
-              multiline
-              rows={8}
-              margin="normal"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          )}
-        />
-      )}
-      {errors.content && <p className={styles.warn}>入力してください</p>}
+    <>
+      <form onSubmit={handleSubmit(registerFirebase)} className={styles.search}>
+        <div className={styles.form}>
+          <p>支援実施日</p>
+          {formatDate(targetDate)}
+        </div>
+        <div className={styles.form}>
+          <InputLabel shrink>支援対象者</InputLabel>
+          <p>{targetName}</p>
+        </div>
+        <div className={styles.form}>
+          <InputLabel shrink>入力ユーザー</InputLabel>
+          <p>{registerName}</p>
+        </div>
+        {!isLoading && (
+          <Controller
+            name="title"
+            control={control}
+            rules={{ required: true }}
+            defaultValue={title}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="支援場面"
+                variant="outlined"
+                placeholder="*必須"
+                margin="normal"
+                helperText="（例）母の日カード作り"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            )}
+          />
+        )}
+        {errors.title && <p className={styles.warn}>入力してください</p>}
+        {!isLoading && (
+          <Controller
+            name="content"
+            control={control}
+            rules={{ required: true }}
+            defaultValue={content}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="内容"
+                variant="outlined"
+                placeholder="*必須"
+                multiline
+                rows={8}
+                margin="normal"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            )}
+          />
+        )}
+        {errors.content && <p className={styles.warn}>入力してください</p>}
 
-      <button
-        className={styles.button}
-        type="button"
-        onClick={() => {
-          reset({
-            title: title,
-            content: content,
-          })
-        }}
-      >
-        リセット
-      </button>
-      <button className={styles.button} type="submit">
-        登録
-      </button>
-    </form>
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => {
+            reset({
+              title: title,
+              content: content,
+            })
+          }}
+        >
+          リセット
+        </button>
+        <button className={styles.button} type="submit">
+          登録
+        </button>
+      </form>
+      <DialogWrapper
+        open={openDialog}
+        title="登録結果"
+        body={alert}
+        setModal={setDialog}
+      />
+    </>
   )
 }
--- utils/repositories/postData.ts
+++ utils/repositories/postData.ts
@@ -1,9 +1,10 @@
 import firebase from '@utils/firebase/ClientApp'
 import { UserInputs } from '../../types/FormModel'
+import { PostModel } from '@utils/entities/PostModel'
 
 export const createTargetUser = async (data: UserInputs): Promise<string> => {
   if (!data) {
-    return 'null'
+    return 'error: no data'
   }
   const targetName = data.lastName + data.firstName
 
@@ -34,7 +35,7 @@ export const createTargetUser = async (data: UserInputs): Promise<string> => {
 
 export const createRegisterUser = async (data: UserInputs): Promise<string> => {
   if (!data) {
-    return 'null'
+    return 'error: no data'
   }
   const registerName = data.lastName + data.firstName
 
@@ -51,6 +52,40 @@ export const createRegisterUser = async (data: UserInputs): Promise<string> => {
         id: timeStamp,
         createdAt: timeStamp,
         updateAt: timeStamp,
+      },
+      { merge: true }
+    )
+
+    await batch.commit()
+
+    return 'success'
+  } catch (e) {
+    return `error: ${e}`
+  }
+}
+
+export const registerPost = async (data: PostModel): Promise<string> => {
+  if (!data) {
+    return 'error: no data'
+  }
+
+  try {
+    const db = firebase.firestore()
+    const batch = db.batch()
+    const ref = db.collection('posts')
+    const timeStamp = getTimeStamp()
+
+    batch.set(
+      ref.doc(data.id),
+      {
+        id: data.id,
+        title: data.title,
+        date: data.date,
+        content: data.content,
+        targetId: data.targetId,
+        registerId: data.registerId,
+        createdAt: timeStamp,
+        updatedAt: timeStamp,
       },
       { merge: true }
     )
