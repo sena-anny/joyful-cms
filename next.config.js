82439b6779782562794ec412b5ac86762ac94f79
sena-anny <cozykoji18@gmail.com>
2021-07-03 01:55:28 +0900

    next.config.jsの修正

--- next.config.js
+++ next.config.js
@@ -1,37 +1,39 @@
 module.exports = {
   async rewrites() {
     if (!process.env.VERCEL_ENV) return {}
-    return [
-      {
-        source: '/posts/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-for',
-            value: process.env.ALLOW_IP,
-          },
-        ],
-        destination: '/posts/:path*',
-      },
-      {
-        source: '/users/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-for',
-            value: process.env.ALLOW_IP,
-          },
-        ],
-        destination: '/users/:path*',
-      },
-      {
-        source: '/posts/:path*',
-        destination: '/',
-      },
-      {
-        source: '/users/:path*',
-        destination: '/',
-      },
-    ]
+    return {
+      fallback: [
+        {
+          source: '/posts/:path*',
+          has: [
+            {
+              type: 'header',
+              key: 'x-forwarded-for',
+              value: process.env.ALLOW_IP,
+            },
+          ],
+          destination: '/posts/:path*',
+        },
+        {
+          source: '/users/:path*',
+          has: [
+            {
+              type: 'header',
+              key: 'x-forwarded-for',
+              value: process.env.ALLOW_IP,
+            },
+          ],
+          destination: '/users/:path*',
+        },
+        {
+          source: '/posts/:path*',
+          destination: '/',
+        },
+        {
+          source: '/users/:path*',
+          destination: '/',
+        },
+      ],
+    }
   },
 }
