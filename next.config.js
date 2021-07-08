ced1f62e2e238d440483a2e370a88206af1dbce0
sena-anny <cozykoji18@gmail.com>
2021-07-09 00:51:33 +0900

    IP制限を解除する

--- next.config.js
+++ next.config.js
@@ -2,56 +2,56 @@ module.exports = {
   async rewrites() {
     if (!process.env.VERCEL_ENV) return {}
     return {
-      beforeFiles: [
-        {
-          source: '/api/:path*/',
-          destination: '/api/:path*',
-        },
-        {
-          source: '/posts/:path*',
-          has: [
-            {
-              type: 'cookie',
-              key: 'x-custom-authorized',
-              value: process.env.AUTH_KEY,
-            },
-          ],
-          destination: '/posts/:path*',
-        },
-        {
-          source: '/users/:path*',
-          has: [
-            {
-              type: 'cookie',
-              key: 'x-custom-authorized',
-              value: process.env.AUTH_KEY,
-            },
-          ],
-          destination: '/users/:path*',
-        },
-        {
-          source: '/posts/:path*',
-          has: [
-            {
-              type: 'cookie',
-              key: 'x-custom-authorized',
-              value: 'false',
-            },
-          ],
-          destination: '/',
-        },
-        {
-          source: '/users/:path*',
-          has: [
-            {
-              type: 'cookie',
-              key: 'x-custom-authorized',
-              value: 'false',
-            },
-          ],
-          destination: '/',
-        },
-      ],
+      // beforeFiles: [
+      //   {
+      //     source: '/api/:path*/',
+      //     destination: '/api/:path*',
+      //   },
+      //   {
+      //     source: '/posts/:path*',
+      //     has: [
+      //       {
+      //         type: 'cookie',
+      //         key: 'x-custom-authorized',
+      //         value: process.env.AUTH_KEY,
+      //       },
+      //     ],
+      //     destination: '/posts/:path*',
+      //   },
+      //   {
+      //     source: '/users/:path*',
+      //     has: [
+      //       {
+      //         type: 'cookie',
+      //         key: 'x-custom-authorized',
+      //         value: process.env.AUTH_KEY,
+      //       },
+      //     ],
+      //     destination: '/users/:path*',
+      //   },
+      //   {
+      //     source: '/posts/:path*',
+      //     has: [
+      //       {
+      //         type: 'cookie',
+      //         key: 'x-custom-authorized',
+      //         value: 'false',
+      //       },
+      //     ],
+      //     destination: '/',
+      //   },
+      //   {
+      //     source: '/users/:path*',
+      //     has: [
+      //       {
+      //         type: 'cookie',
+      //         key: 'x-custom-authorized',
+      //         value: 'false',
+      //       },
+      //     ],
+      //     destination: '/',
+      //   },
+      // ],
     }
   },
 }
